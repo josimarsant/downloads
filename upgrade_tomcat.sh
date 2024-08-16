6c77587aa243db742e3dc9132f2db4a22fb396f5
josimarsant <113646862+josimarsant@users.noreply.github.com>
2024-08-16 15:35:47 -0400

    Update upgrade_tomcat.sh

--- upgrade_tomcat.sh
+++ upgrade_tomcat.sh
@@ -10,7 +10,6 @@ move() {
     echo "Destination: $destinationfolder"
     mkdir $destinationfolder
     find $1 -mindepth 1 -maxdepth 1 -not -name $2 -not -name $3 -not -name $4 -exec mv {} $destinationfolder \;
-
 }
 
 backup() {
@@ -32,9 +31,29 @@ backup() {
     fi
 }
 
+
+
 update() {
-    systemdfolder="/etc/systemd/system"
-    tomcatversion="9.0.89"
+
+myArray=$(tomcatsvc)
+
+if [[ -z "$myArray" ]]; then
+    echo "No Tomcat services found."
+    exit 1
+fi
+
+echo "Tomcat services:"
+echo "$myArray" | nl -w 2 -s '. '
+
+echo "Digite quais versões serão atualizadas separado por espaços.(e.g., 1 2 3):"
+read -p "Opção escolhida: " choices
+
+for choice in $choices; do
+    tomcatsvcname=$(echo "$myArray" | sed -n "${choice}p")
+    if [[ -n "$tomcatsvcname" ]]; then
+        echo "tomcat escolhido: $choice:"
+        systemdfolder="/etc/systemd/system"
+    tomcatversion="9.0.93"
     cat $systemdfolder/$tomcatsvcname.service | grep 'WorkingDirectory' | cut -d= -f2 | sed 's/[[:space:]]//g' | grep running
     if [ $? -eq 0 ]; then
         #Running word found!
@@ -145,36 +164,102 @@ EOF
         echo "skip"
         exit 1
     fi
+    else
+        echo "Invalid choice: $choice"
+    fi
 
+done
+}
+
+install() {
+    echo "####  Java must be installed ######"
+    echo "Especificação para tomcat: (e.g. tomcat1)"
+    read -p "R:" tomcatsvcname
+
+    echo "Defina a porta para o service: (e.g. 8081)"
+    read -p "Port: " tomcatport
+
+    echo "Defina a porta para o server: (e.g. 8005)"
+    read -p "Port: " serverport
+
+    echo "Instalar como $tomcatsvcname na porta $tomcatport? (y/n)"
+    read -p "R: " ans
+    if [ $ans == "y" ]; then
+        systemdfolder="/etc/systemd/system"
+        tomcatversion="9.0.93"
+        tomcatdir=/usr/local/$tomcatsvcname
+        tomcatrunning="$tomcatdir/running"
+        java_home=$(java -XshowSettings:properties -version 2>&1 >/dev/null | grep 'java.home' | cut -d= -f2 | sed 's/^[[:blank:]]*//')
+        instprocess        
+    fi
+}
+
+instprocess() {
+
+    #Install Tomcat commands:
+    #dnf install java-1.8.0-openjdk.x86_64 -y
+    mkdir $tomcatdir && cd $tomcatdir && sudo mkdir running >/dev/null && 
+    sudo mkdir $tomcatversion && cd $tomcatversion
+    pwd
+    sudo wget https://archive.apache.org/dist/tomcat/tomcat-9/v$tomcatversion/bin/apache-tomcat-$tomcatversion.tar.gz
+    sudo tar -xzf apache-tomcat-$tomcatversion.tar.gz --strip-components=1
+    id -u tomcat &>/dev/null || sudo useradd -r -m -U -d $tomcatdir -s /bin/false tomcat
+    sudo ln -s $tomcatdir/$tomcatversion/* $tomcatrunning
+    sudo chown -R tomcat: $tomcatdir*
+    sudo chmod +x $tomcatrunning/bin/*.sh
+    #change the tomcat port:
+    sed -i -E "s/(<Connector port=\")([0-9]+)(\" protocol=\"HTTP\/1\.1\")/\1${tomcatport}\3/" $tomcatrunning/conf/server.xml
+    sed -i -E "s/(<Server port=\")([0-9]+)(\" shutdown=\")/\1${serverport}\3/" $tomcatrunning/conf/server.xml
+    cat <<EOF >$systemdfolder/$tomcatsvcname.service
+[Unit]
+Description=Tomcat 9 se{{ let container
+After=network.target
+
+[Service]
+Type=forking
+WorkingDirectory=$tomcatrunning
+User=tomcat
+Group=tomcat
+Environment="JAVA_HOME=$java_home"
+Environment="JAVA_OPTS=-Djava.security.egd=file:///dev/urandom"
+Environment="CATALINA_BASE=$tomcatrunning"
+Environment="CATALINA_HOME=$tomcatrunning"
+Environment="CATALINA_PID=$tomcatrunning/temp/$tomcatsvcname.pid"
+Environment="CATALINA_OPTS=-Xms512M -Xmx1024M -server -XX:+UseParallelGC"
+ExecStart=$tomcatrunning/bin/startup.sh
+ExecStop=$tomcatrunning/bin/shutdown.sh
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+    systemctl daemon-reload
+    sudo systemctl enable $tomcatsvcname
+
+    sudo systemctl start $tomcatsvcname
+    sudo systemctl status $tomcatsvcname
+
+    echo "Installation done!"
+
+    #SELinux
+    #ausearch -c '(artup.sh)' --raw | audit2allow -M tomcat.service
+    #semodule -i tomcat.service.pp
+    #erro para o arquivo startup.sh **verificar
 }
 
 tomcatsvc() {
     systemctl list-unit-files --type=service | grep \.service | grep tomcat | awk -F '.service' '{print $1}'
 }
 
-myArray=$(tomcatsvc)
+    echo "Deseja Instalar ou Atualizar tomcat? I: Instalar, A: Atualizar"
+    read -p "R: " mode
 
-if [[ -z "$myArray" ]]; then
-    echo "No Tomcat services found."
-    exit 1
-fi
-
-echo "Tomcat services:"
-echo "$myArray" | nl -w 2 -s '. '
-
-echo "Digite quais versões serão atualizadas separado por espaços.(e.g., 1 2 3):"
-read -p "Opção escolhida: " choices
-
-for choice in $choices; do
-    tomcatsvcname=$(echo "$myArray" | sed -n "${choice}p")
-    if [[ -n "$tomcatsvcname" ]]; then
-        echo "tomcat escolhido: $choice:"
-        update
+    if [ $mode == "I" ]; then
+        install
     else
-        echo "Invalid choice: $choice"
+        update
     fi
 
-done
 
 #TOdo:
 # test if runnning
